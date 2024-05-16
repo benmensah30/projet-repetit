329e090dea12ec0c8c70446f1dd84e798e0ec65a
= <157025874+benmensah30@users.noreply.github.com>
2024-05-16 14:25:06 +0100

    update2

--- src/main/java/com/example/repetit/controllers/SignUp.java
+++ src/main/java/com/example/repetit/controllers/SignUp.java
@@ -1,4 +1,13 @@
 package com.example.repetit.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class SignUp {
+
+    @FXML
+    private TextField id;
+
+    id.setOpacity(5);
+
 }
