d17c5a89f016ab0b0bffc21daa1a984df80803e0
= <=>
2024-05-17 10:22:09 +0000

    update8

--- src/main/java/com/example/repetit/controllers/signUp2.java
+++ src/main/java/com/example/repetit/controllers/signUp2.java
@@ -5,32 +5,32 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.Objects;
 
-public class SignUp {
+public class signUp2 {
 
     @FXML
-    private TextField id;
+    private PasswordField password;
 
     @FXML
-    private ChoiceBox<?> choice;
+    private TextField username;
 
     //methode qui permet de passer de la page de connexion a la page d'inscription
-    public void signInRedirect() throws IOException {
+    public void signInConnect() throws IOException {
 
 
-        Parent good = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("signUp2.fxml")));
+        Parent good = FXMLLoader.load(Objects.requireNonNull(HelloApplication.class.getResource("connection.fxml")));
         Stage stg = new Stage();
         Scene scene = new Scene(good);
         stg.setScene(scene);
         stg.show();
 
-        Stage connectscene = (Stage) choice.getScene().getWindow();
+        Stage connectscene = (Stage) username.getScene().getWindow();
         connectscene.close();
     }
 
